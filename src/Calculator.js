832aba747b4ab6fb9edebf4055ebe826c99135d6
JustinT9 <jus81818@yahoo.com>
2023-07-21 15:20:09 -0400

    fixing bugs with +- input

--- src/Calculator.js
+++ src/Calculator.js
@@ -8,7 +8,7 @@ function Calculator() {
     // expression for the calculation
     const [expression, setExpression] = useState([]); 
     // a number to input into the expression 
-    const [num, setNum] = useState(""); 
+    const [num, setNum] = useState("0"); 
     // current operation 
     const [op, setOp] = useState();  
     // the number used to add if equals operation is spammed 
@@ -26,13 +26,15 @@ function Calculator() {
                 setExpression((oldExpression) => { 
                     return [eval(oldExpression.slice(0, oldExpression.length-1).join("")), oldExpression[oldExpression.length-1]]
                 })
-            // otherwise if equals is used 
             } else {
                 if (op === "=") {
                     setView(eval(expression.join("")));
                     setExpression((oldExpression) => { return [eval(oldExpression.join("")), saved.op]});
-                }
+                } 
             }
+        } else if (op === "+-") {
+            setView(expression[0]); 
+            setSaved((oldSave) => { return {...oldSave, num: expression[0]*-1}}); 
         }
 
         console.log(expression);
@@ -47,16 +49,20 @@ function Calculator() {
         if (symbol === "C") {
             setExpression([]);
             setView("0"); 
-            setNum(""); 
+            setNum("0"); 
             setOp("");
             setSaved({num: null, op: null});
         } 
 
         // otherwise if a number is inputted 
-       else if (symbol === "1" || symbol === "2" || symbol === "3" || 
+       else if (symbol === "0" || symbol === "1" || symbol === "2" || symbol === "3" || 
         symbol === "4" || symbol === "5" || symbol === "6" || symbol === "7" || 
         symbol === "8" || symbol === "9") {
-            setNum((oldNum) => { return oldNum + symbol })
+            if (num === "0") {
+                setNum(symbol);
+            } else {
+                setNum((oldNum) => { return oldNum + symbol });
+            }
 
         // if an operation is inputted 
         } else {
@@ -69,8 +75,9 @@ function Calculator() {
                     setNum("");
                 // to change the sign or operation 
                 } else if (expression.length > 0) {
+                    // if the number is negative and a subtraction operation is conducted 
                     if (symbol === "-" && expression[expression.length-1] === "-" || 
-                    symbol === "-" && parseInt(expression[0]) < 0) {
+                    symbol === "-" && parseFloat(expression[0]) < 0) {
                         setExpression((oldExpression) => { return [...oldExpression.slice(0, expression.length-1), "+"] } );
                         setSaved( {num: -1*expression[0], op: "+"} )
                     } else {
@@ -91,7 +98,19 @@ function Calculator() {
                         setNum(""); 
                     }
                 } else if (symbol === "+-") {
-                    setNum((parseInt(num)*-1).toString());
+                    if (num !== "") {
+                        if (parseFloat(num) % 1 == 0) {
+                            setNum((parseInt(num)*-1).toString());
+                        } else {
+                            setNum((parseFloat(num)*-1.00).toString());
+                        }
+                    } else {
+                        if (expression[0] % 1 == 0) {
+                            setExpression([expression[0]*-1, expression[expression.length-1]])
+                        } else {
+                            setExpression([expression[0]*-1.00, expression[expression.length-1]])
+                        }
+                    }
                 } else if (symbol === ".") {
                     setNum((oldNum) => { return oldNum + "."} )
                 }
