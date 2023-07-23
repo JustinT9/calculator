d2025dc6bab320bcb05906459082cb618b3ca9fb
Justin Tran <jus81818@yahoo.com>
2023-07-23 12:07:54 -0400

    now floats and some other little errors

--- src/Calculator.js
+++ src/Calculator.js
@@ -22,7 +22,7 @@ function Calculator() {
         
         // otherwise if a number is calculated then display that answer...  
         } else if (expression.length > 2) {
-            // display calculations with operations other than =, +-, and . 
+            // display calculations with operations +, -, *, /, %
             // and this if the normal operations are clicked to compute the answer 
             if (op !== "=") {
                 // since two elements are added to an expression then you must get an expression 
@@ -32,17 +32,21 @@ function Calculator() {
                 setExpression((oldExpression) => { 
                     return [eval(oldExpression.slice(0, oldExpression.length-1).join("")), saved.op]
                 })
+                // display calculations with operation +-, =, . 
             } else {
+                // evaluate with = 
                 if (op === "=") {
                     setView(eval(expression.join("")));
                     setExpression((oldExpression) => { return [eval(oldExpression.join("")), saved.op]});
                 } 
             }
+        // initially... 
         } else if (op === "+-") {
             setView(expression[0]); 
             setSaved((oldSave) => { return {...oldSave, num: expression[0]*-1}}); 
         }
 
+        // just to test 
         console.log("EXPRESSION:", expression);
         console.log("NUMBER:", num); 
         console.log("OPERATION:", op); 
@@ -72,32 +76,42 @@ function Calculator() {
                 setNum((oldNum) => { return oldNum + symbol });
             }
         }
-         
+        
         // if an operation is inputted 
         else {
             // normal operations such as +, -, *, -, %
             if (symbol !== "+-" && symbol !== "." && symbol !== "=") {
                 // if there is a number inputted then the expression can be updated 
                 if (num !== "") {
-                    setExpression((oldExpression) => { return [...oldExpression, num, symbol] } ); 
-                    // saved if the = operation is used for an incomplete expression 
-                    // e.g. 5 + ... --> 5 + 5 --> 10 + ... --> 10 + 5 ... 
-                    setSaved({num: num, op: symbol});
+                    setExpression((oldExpression) => { 
+                        // negate negatives if possible 
+                        if (oldExpression[oldExpression.length-1] === "-" && parseFloat(num) < 0) {
+                            setSaved({num: -1*parseInt(num), op: symbol});
+                            return [oldExpression[0], "+", -1*parseInt(num), symbol];
+                        // otherwise normally concate the expression 
+                        } else {
+                            // saved if the = operation is used for an incomplete expression 
+                            // e.g. 5 + ... --> 5 + 5 --> 10 + ... --> 10 + 5 ... 
+                            setSaved({num: num, op: symbol});
+                            return [...oldExpression, num, symbol]; 
+                        }
+                    }); 
                     // must be reset so another new number can be concatenated 
                     setNum("");
 
                 // if the user decides to change the operation within the expression to +, -, *, /, or % 
                 } else if (expression.length > 0) {
-                    // if the user inputs negative symbol and the current operation is already - the negate **fix** 
-                    // this case is only for equals if the number is already negative and the symbol inputted is negative 
-                    if (symbol === "-" && expression[expression.length-1] === "-" || 
-                    symbol === "-" && parseFloat(expression[0]) < 0) {
+                    // if the user inputs negative symbol and the number is already negative then negation is needed 
+                    // since its in the context of equals operation 
+                    if (symbol === "-" && parseFloat(expression[0]) < 0) {
                         setExpression((oldExpression) => { return [...oldExpression.slice(0, expression.length-1), "+"] } );
+                        // update for the equals operation 
                         setSaved( {num: -1*expression[0], op: "+"} )
                     
                     // otherwise if there is no negative sign to negate then just replace the operation normally 
                     } else {
                         setExpression((oldExpression) => { return [...oldExpression.slice(0, expression.length-1), symbol] } ); 
+                        // in case the equals operation is used 
                         setSaved( {num: expression[0], op: symbol } )
                     }
                 }
@@ -106,33 +120,61 @@ function Calculator() {
             } else {
                 // if there is an expression to evaluate with the equals operation 
                 if (symbol === "=" && expression.length > 0) {
-                    // if its an operation such as (56 +) and equals is used 
+                    // if its an operation such as (56 +) and equals is used and is spammed where 
+                    // there is no number inputted then operate with the same number 
                     if (!Number.isInteger(expression[expression.length-1]) && num === "") {
                         setExpression((oldExpression) => { return [...oldExpression, saved.num]})
 
                     // otherwise if its a normal operation on expression such as 56 + 56 
                     } else {
-                        setExpression((oldExpression) => { return [...oldExpression, num]} );
-                        setSaved((oldSave) => { return {...oldSave, num: num}}); 
+                        setExpression((oldExpression) => {
+                            // negate  
+                            if (oldExpression[oldExpression.length-1] === "-" && parseFloat(num) < 0) {
+                                setSaved({num: -1*parseInt(num), op: "+"}); 
+                                return [oldExpression[0], "+", -1*parseInt(num)]; 
+                            } else {
+                                setSaved((oldSave) => { return {...oldSave, num: num}}); 
+                                return [...oldExpression, num];  
+                            }
+                        }); 
+                        // must save the number that was added 
                         setNum(""); 
                     }
                 
-                } else if (symbol === "+-") {
+                // when it comes to negate, must account for two cases, negating a number that was 
+                // inputted and negating a result and updating the operations as a result 
+                } else if (symbol === "+-") {   
+                    // if number inputted 
                     if (num !== "") {
-                        if (parseFloat(num) % 1 == 0) {
+                        // must consider if its a float or not to preserve value 
+                        if (parseFloat(num) % 1 == 0) { 
                             setNum((parseInt(num)*-1).toString());
+                            setSaved((oldSaved) => { return {...oldSaved, num: (parseInt(num)*-1).toString()}});
                         } else {
                             setNum((parseFloat(num)*-1.00).toString());
-                        }
+                            setSaved((oldSaved) => { return {...oldSaved, num: (parseFloat(num)*-1.00).toString()}});
+
+                        } 
+                    
+                    // otherwise it must be a result 
                     } else {
+                        // must consider if its a float or not to preserve value 
                         if (expression[0] % 1 == 0) {
                             setExpression([expression[0]*-1, expression[expression.length-1]])
+                            setSaved((oldSaved) => { return {...oldSaved, num: expression[0]*-1}})
                         } else {
                             setExpression([expression[0]*-1.00, expression[expression.length-1]])
-                        }
+                            setSaved((oldSaved) => { return {...oldSaved, num: expression[0]*-1.00}})
+                        } 
                     }
-                } else if (symbol === ".") {
-                    setNum((oldNum) => { return oldNum + "."} )
+
+                } else if (symbol === "." && !num.includes(".")) {
+                    if (num !== "") {
+                        setNum((oldNum) => { return oldNum + "."})
+                    // if "." is inputted and there is no digit inputted yet 
+                    } else {
+                        setNum(0 + ".")
+                    }
                 }
             }
             // need to get the operation to render the view 
